cc4c5e745a91c334a38d97681883553a9ae857cb
realchief <stryokka@gmail.com>
2018-03-07 18:27:54 +0100

    adding functions for upload csv

--- login/urls.py
+++ login/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-
+    url(r'^upload/$', {'template_name': 'model_form_upload.html'}, name='logout'),
 ]
